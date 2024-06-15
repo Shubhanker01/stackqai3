5240a8cf8ffec47f4dc79b2e884f65a9d9ff7e51
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-15 18:09:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Components/Home'
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Redirect from './Components/Redirect';
+import Main from './Components/Main';
+import History from './Components/History';
+
+function App() {
+  return (
+    <>
+      
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/signup' element={<Signup></Signup>}></Route>
+            <Route path='/redirect' element={<Redirect></Redirect>}></Route>
+            <Route path='/main' element={<Main></Main>}></Route>
+            <Route path='/history' element={<History></History>}></Route>
+          </Routes>
+        </Router>
+      
+
+
+    </>
+  )
+}
+
+export default App
--- quesans.js
+++ quesans.js
@@ -6,23 +6,43 @@ const user = require('../Schemas/user')
 
 router.use(express.json())
 
-router.post('/',async function(req,res){
-    try{
-        const data = await user.findOne({email:req.body.email}).exec()
+// save question answers
+router.post('/', async function (req, res) {
+    try {
+        const data = await user.findOne({ email: req.body.email }).exec()
         await quesans.create({
-            "user_id":data._id,
-            "question":req.body.question,
-            "answer":req.body.answer
-        }).then(()=>{
+            "user_id": data._id,
+            "question": req.body.question,
+            "answer": req.body.answer,
+            "date":Date
+        }).then(() => {
             res.send("Saved successfully")
-        }).catch((err)=>{
+        }).catch((err) => {
             res.send(err)
         })
-        
+
     }
-    catch(err){
+    catch (err) {
         res.send(err)
     }
 })
 
+router.post('/history', async function (req, res) {
+    try {
+        let data = await user.findOne({ email: req.body.email })
+        let response = await quesans.find({ user_id: data._id })
+        let results = response.map(function (result) {
+            let obj = {
+                "_id":result._id,
+                "question": result.question,
+                "answer": result.answer
+            }
+            return obj
+        })
+        res.send(results)
+    }
+    catch (err) {
+        res.send(err)
+    }
+})
 module.exports = router
