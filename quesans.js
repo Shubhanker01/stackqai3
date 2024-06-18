c74ed93224d583b77ea91536699d6d7c499df8f9
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-18 13:22:25 +0530

    Add files via upload

--- quesans.js
+++ quesans.js
@@ -15,7 +15,7 @@ router.post('/', async function (req, res) {
             "user_id": data._id,
             "question": req.body.question,
             "answer": req.body.answer,
-            "date":date
+            "date": date
         }).then(() => {
             quesans.markModified('date')
             quesans.bulkSave()
@@ -35,15 +35,16 @@ router.post('/history', async function (req, res) {
         let data = await user.findOne({ email: req.body.email })
         let response = await quesans.find({ user_id: data._id })
         let results = response.map(function (result) {
-            let weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
-            let monthDay = new Array("January","Feburary","March","April","May","June","July","August","September","October","November","December")
+            let weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+            let monthDay = new Array("January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
             let day = result.date.getDay()
             let newDate = result.date.toLocaleDateString()
             let obj = {
-                "_id":result._id,
+                "_id": result._id,
+                "user_id":result.user_id,
                 "question": result.question,
                 "answer": result.answer,
-                "date":`${weekday[day]}, ${newDate}`
+                "date": `${weekday[day]}, ${newDate}`
             }
             return obj
         })
@@ -53,4 +54,29 @@ router.post('/history', async function (req, res) {
         res.send(err)
     }
 })
+
+// delete a specific question answer
+router.delete('/delete/:id', async function (req, res) {
+    try {
+        await quesans.findByIdAndDelete(req.params.id)
+        res.send("deleted successfully")
+    }
+    catch (err) {
+        res.send(err)
+    }
+})
+
+// delete all the chats of the user with the bot
+router.delete('/delete/all/:userId', async function (req, res) {
+    try {
+        await quesans.deleteMany({ user_id: req.params.userId })
+        res.send("All the documents successfully deleted")
+    }
+    catch (err) {
+        res.send(err)
+    }
+})
+
+
+
 module.exports = router
