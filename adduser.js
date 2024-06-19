fae8101232006b50f32c2e4238efb4950b591a1e
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-19 20:44:13 +0530

    Add files via upload

--- adduser.js
+++ adduser.js
@@ -4,15 +4,25 @@ router.use(express.json())
 const user = require('../Schemas/user')
 
 router.post('/', async (req, res) => {
-    await user.create({
-        "name": req.body.name,
-        "email": req.body.email,
-        "is_verified": req.body.is_verified
-    }).then(response => {
-        res.send(response)
-    }).catch(err => {
-        console.log(err)
-    })
+    // check if the user already exists
+    const query = await user.findOne({ email: req.body.email }).exec()
+    // query==null means user does not exist
+    if (query == null) {
+        await user.create({
+            "name": req.body.name,
+            "email": req.body.email,
+            "is_verified": req.body.is_verified
+        }).then(response => {
+            res.send(response)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    else {
+        res.send(query)
+    }
+
+
 })
 
 
