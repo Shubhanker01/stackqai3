c035894a38ac54adcdb1bc6350535ff4165ceab7
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-20 20:32:51 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -7,23 +7,23 @@ import Signup from './Components/Signup';
 import Redirect from './Components/Redirect';
 import Main from './Components/Main';
 import History from './Components/History';
+import RedirectLogout from './Components/RedirectLogout';
 
 function App() {
   return (
     <>
-      
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/login' element={<Login></Login>}></Route>
-            <Route path='/signup' element={<Signup></Signup>}></Route>
-            <Route path='/redirect' element={<Redirect></Redirect>}></Route>
-            <Route path='/main' element={<Main></Main>}></Route>
-            <Route path='/history' element={<History></History>}></Route>
-          </Routes>
-        </Router>
-      
 
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
+          <Route path='/redirect' element={<Redirect></Redirect>}></Route>
+          <Route path='/main' element={<Main></Main>}></Route>
+          <Route path='/history' element={<History></History>}></Route>
+          <Route path='/redirectlogout' element={<RedirectLogout />}></Route>
+        </Routes>
+      </Router>
 
     </>
   )
