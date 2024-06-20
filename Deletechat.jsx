11748c0eb6ef2676d062d431bda9df6bb2eb0191
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-20 20:31:30 +0530

    Add files via upload

--- Deletechat.jsx
+++ Deletechat.jsx
@@ -31,12 +31,12 @@ export default function Deletechat(props) {
                                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAvklEQVR4nO3VwQnCMBjF8Qe6hnYRO4FSPNcJ9FxBF9AJdAKdQCcwE9gJdAOPFaFKoFfBvPdJEPuH3JLvR3IJ0PZ5YwAZInQHUH1reNYMfwauin0NR2DvlosFn5ib/1c3g2f2M4IrDeAzAx8N4AMDbw3gDQMvDeAFA08M4JyBUwN4wMB9A7jHwB0ADwH1Z7sguwrwBUIu1uewF+CdAq8FeKXAMwGeKvBIgIcKnACoCbRuzkrNA/9mv7dQ0bbf7wXRGEWDOGx/GAAAAABJRU5ErkJggg==" />
                             </button>
                         </div>
-                        <div className="fixed w-full h-full z-1 left-0 top-0 bg-[rgba(0,0,0,0.4)]">
-                            <div className="m-auto mt-[20px] h-[150px] w-[400px] bg-zinc-200 rounded-lg">
-                                <h1 className="text-3xl text-bold ml-[10px] mt-[10px]">Delete Chat</h1>
-                                <p className="text-xl ml-[10px] mt-[10px]">Are you sure you want to delete this chat?</p>
-                                <button className="bg-green-100 mt-[10px] p-2 text-bold text-xl ml-[10px] rounded-lg" onClick={()=>{deleteChat()}}>Yes</button>
-                                <button className="bg-red-100 rounded-lg text-xl p-2 text-bold ml-[10px]" onClick={()=>{showPopup(false)}}>No</button>
+                        <div className="fixed w-full h-full z-10 left-0 top-[0px] bg-[rgba(0,0,0,0.4)]">
+                            <div className="m-auto mt-[225px] h-[150px] w-[400px] bg-slate-200 rounded-lg">
+                                <h1 className="text-3xl text-bold ml-[15px] mt-[15px]">Delete Chat</h1>
+                                <p className="text-xl ml-[15px] mt-[15px]">Are you sure you want to delete this chat?</p>
+                                <button className="bg-sky-700 mt-[15px] p-2 font-bold text-lg ml-[15px] rounded-lg text-slate-100" onClick={()=>{deleteChat()}}>Yes</button>
+                                <button className="bg-red-700 rounded-lg text-lg p-2 font-bold ml-[15px] text-slate-100" onClick={()=>{showPopup(false)}}>No</button>
                             </div>
                         </div>
 
