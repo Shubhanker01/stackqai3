800c332923bf7d44d8479cf86d7140af2e974e89
Shubhanker01 <87701957+Shubhanker01@users.noreply.github.com>
2024-06-21 20:55:28 +0530

    Add files via upload

--- Redirect.jsx
+++ Redirect.jsx
@@ -15,8 +15,8 @@ function Redirect() {
 
   return (
     <>
-      <div className="mt-[15px]">
-        <img src="https://i.pinimg.com/originals/d8/16/4b/d8164b6fb6eebeabbd54e842971c050f.gif" alt="" className="w-[200px] h-[200px] m-auto" />
+      <div className="">
+        <img src="https://i.pinimg.com/originals/d8/16/4b/d8164b6fb6eebeabbd54e842971c050f.gif" alt="" className="w-[200px] h-[200px] m-auto pt-[30px]" />
         <h1 className="text-3xl text-center font-bold">You have successfully signed in</h1>
         <img src={data.img} className="m-auto rounded-full"></img>
         <p className="text-center text-xl pt-[5px] font-semibold">Welcome {data.name}</p>
